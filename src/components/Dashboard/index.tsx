8ba111df1f036b1e2ac89a19107f32807f8c7a25
Matheus Abreu <theus.santosab@gmail.com>
2022-03-30 20:27:08 -0300

    create dashboard component

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import Summary from "../Summary";
+import * as S from "./styles";
+
+export default function Dashboard() {
+  return (
+    <S.Container>
+      <Summary />
+    </S.Container>
+  );
+}
