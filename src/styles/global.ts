c32d1bed66c02a561cdc90dcb782b9f37a4cf79f
Matheus Abreu <theus.santosab@gmail.com>
2022-03-31 19:59:56 -0300

    create transactionsTable component

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,9 +9,9 @@ export const GlobalStyle = createGlobalStyle`
         --blue-light: #6933FF;
         
         --text-title: #363F5F;
-        --text-body: #969CB3;
+        --text-body: #969CB2;
         
-        --background: #f8f2f5;
+        --background: #F0F2F5;
         --shape: #FFFFFF;
     }
     
