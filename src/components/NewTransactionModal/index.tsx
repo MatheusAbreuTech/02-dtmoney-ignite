f91597410e8dd3e4df098fb53aefe4a0f30f7e62
Matheus Abreu <theus.santosab@gmail.com>
2022-04-04 22:03:54 -0300

    function created to add new transaction and list all transactions

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,9 @@
+import { FormEvent, useState } from "react";
 import Modal from "react-modal";
 import closeImg from "../../assets/close.svg";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import { api } from "../../services/api";
 import * as S from "./styles";
 
 interface NewTransactionModalProps {
@@ -11,6 +15,22 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
+  const [title, setTitle] = useState("");
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState("");
+  const [type, setType] = useState("deposit");
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
+
+    api.post("/transactions", {
+      title,
+      value,
+      category,
+      type,
+    });
+  }
+
   return (
     <Modal
       isOpen={isOpen}
@@ -24,12 +44,49 @@ export function NewTransactionModal({
         <img src={closeImg} alt="" />
       </button>
 
-      <S.Container>
+      <S.Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar transação</h2>
 
-        <input type="text" placeholder="Título" />
-        <input type="number" placeholder="Valor" />
-        <input type="text" placeholder="Categoria" />
+        <input
+          type="text"
+          placeholder="Título"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+
+        <input
+          type="number"
+          placeholder="Valor"
+          value={value}
+          onChange={(event) => setValue(Number(event.target.value))}
+        />
+
+        <S.TransactionTypeContainer>
+          <S.RadioBox
+            type="button"
+            isActive={type === "deposit"}
+            activeColor={"green"}
+            onClick={() => setType("deposit")}>
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </S.RadioBox>
+
+          <S.RadioBox
+            type="button"
+            isActive={type === "withdraw"}
+            activeColor={"red"}
+            onClick={() => setType("withdraw")}>
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </S.RadioBox>
+        </S.TransactionTypeContainer>
+
+        <input
+          type="text"
+          placeholder="Categoria"
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        />
 
         <button type="submit">Cadastrar</button>
       </S.Container>
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -47,3 +47,49 @@ export const Container = styled.form`
     }
   }
 `;
+
+export const TransactionTypeContainer = styled.div`
+  margin: 1rem 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 0.5rem;
+`;
+
+interface RadioBoxProps {
+  isActive: boolean;
+  activeColor: "green" | "red";
+}
+
+const colors = {
+  green: "rgba(18, 164, 84, 0.1);",
+  red: "rgba(230, 46, 77, 0.1);",
+};
+
+export const RadioBox = styled.button<RadioBoxProps>`
+  height: 4rem;
+  border: 1px solid #d7d7d7;
+  border-radius: 0.25rem;
+
+  background: ${(props) =>
+    props.isActive ? colors[props.activeColor] : "transparent"};
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    border-color: #aaa;
+  }
+
+  img {
+    width: 20px;
+    height: 20px;
+  }
+
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
+  }
+`;
